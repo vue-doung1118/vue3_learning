f83b2a8d570881a479fc2276f2110ddfa1e59ad3
Barry Yu <doung1118@gmail.com>
2020-12-30 21:39:07 +0800

    git first

---
+++ app.js
@@ -0,0 +1,17 @@
+// console.log('hello ,vue,thank')
+
+
+const app =Vue.createApp( 
+
+  //Vue is Capitalize （ Vue )
+
+  //data and functions 
+  {template:'<h2>this is mount to the #app with id="app" </h2> '
+})
+
+
+app.mount('#app') 
+
+//app透過mount傳送器() , index.html 內的 div id = "app "  和 ＃app match 可以互相對應
+
+//test 
